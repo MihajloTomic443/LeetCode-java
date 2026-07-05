bbf8385687de797b43300e240222c41bd976b0d3
Mihajlo Tomic <mihajlotomic443@gmail.com>
2026-07-05 18:48:27 +0200

    TwoSum #1

---
+++ src/arrays/TwoSum.java
@@ -0,0 +1,40 @@
+package arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+LeetCode #1 - Two sum
+Difficulty: Easy
+Topic: HashMap
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+Idea:
+Store visited numbers in HashMap where
+key = number
+value = index.
+If target-current exists in map,
+return indices.
+
+Learned:
+- HashMap lookup is O(1)
+- Store value->index mapping
+- Check before insert to avoid using same element twice
+
+Solved: 2026-07-06
+*/
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length;i++){
+            int potreban = target - nums[i];
+            if(map.containsKey(potreban)){
+                return new int[]{i, map.get(potreban)};
+            }
+            map.put(nums[i],i);
+
+        }
+        return null;
+    }
+}
